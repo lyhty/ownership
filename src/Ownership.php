619b02bd3c122564897574b65060b1fbfd745afe
Matti Suoraniemi <37704147+sirmathays@users.noreply.github.com>
2022-07-19 21:02:32 +0200

    Update Ownership trait

--- src/Ownership.php
+++ src/Ownership.php
@@ -7,16 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 trait Ownership
 {
     /**
-     * Return boolean value whether the model instance owns given Model.
+     * Return boolean value whether the model instance owns the given Model.
      *
      * @param \Illuminate\Database\Eloquent\Model $model
-     * @param string $key
+     * @param string|null $key
      * @return bool
      */
-    public function owns(Model $model, string $key): bool
+    public function owns(Model $model, string $key = null): bool
     {
         $key ??= $this->getForeignKey();
 
         return $this->getKey() == $model->{$key};
+    }
+    
+    /**
+     * Return boolean value whether the model instance doesn't own the given Model.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string|null $key
+     * @return bool
+     */
+    public function doesntOwn(Model $model, string $key = null): bool
+    {
+        return !$this->owns($model, $key);
     }
 }
